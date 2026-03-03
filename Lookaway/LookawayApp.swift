82a4442c7b493b98e523e17005f03b9f89e2c006
minkonaing99 <mkn.minko.naing99@gmail.com>
2026-03-03 19:20:37 +0700

    Initial Commit

---
+++ Lookaway/LookawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookawayApp.swift
+//  Lookaway
+//
+//  Created by Aurora on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LookawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
